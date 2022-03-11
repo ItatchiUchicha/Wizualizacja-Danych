1fba3a3063739b6141e133b6f51ec76bc7504471
Kacper Korzeniewski <93936431+ItatchiUchicha@users.noreply.github.com>
2022-03-11 22:35:27 +0100

    Add files via upload

---
+++ Zadania domowe lab1.py
@@ -0,0 +1,70 @@
+import math
+#Zad1 Pierwszy skrypt
+# liczba1 = 3
+# liczba2 = 4
+# print(liczba1,liczba2)
+# tekst1 = "Hello"
+# tekst2 = "World"
+# print(tekst1,tekst2)
+# lista1 = [1,2,3,'abc']
+# lista2 = [2.4,8,'H']
+# print(lista1,lista2)
+# krotka1 = (1,2,3,'abc')
+# krotka2 = ('HYZ',2.3,4)
+# print(krotka1,krotka2)
+# slownik1 = {'Poland':'Warsaw'}
+# slownik2 = {'Russia':'Moscow'}
+# print(slownik1,slownik2)
+#Zad2 Kalkulator
+# a = 10
+# b = 5
+# c = 2
+# dzielenie = a / b
+# print(dzielenie)
+# dodawanie = c + b
+# print(dodawanie)
+# dzielenie_calkowite = a // b
+# print(dzielenie_calkowite)
+# reszta = c % b
+# print(reszta)
+# potega = c ** a
+# print(potega)
+#Zad3 Operatory przyrostkowe
+# z = 3
+# z += 1
+# print(z)
+# x = 5
+# x -= 2
+# print(x)
+# c = 8
+# c *= 2
+# print(c)
+# v = 9
+# v /= 3
+# print(v)
+# b = 7
+# b **= 2
+# print(b)
+# n = 6
+# n %= 5
+# print(n)
+#Zad4 Obliczanie wyrażeń
+# print(math.exp(10))
+#
+# print(math.fabs(3.55))
+# print(math.fabs(4.80))
+#Zad5 Imie i nazwisko
+# imie = 'KACPER '
+# nazwisko = 'KORZENIEWSKI'
+# print(imie.capitalize()+nazwisko.capitalize())
+#Zad6 Zmienna string
+# slowa = 'Jade do Miami na festiwal la la la la la la'
+# print(slowa.count("la"))
+#Zad7 Zmienna łańcuchowa
+# slowo = 'Kapitol'
+# znak = slowo[1]
+# print(slowo[0])
+# print(slowo[6])
+#Zad8 Dzielenie zmiennej
+#slowa = 'Jade do Miami na festiwal la la la la la la'
+#print(slowa.split())
