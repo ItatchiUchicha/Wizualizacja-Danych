2a9b9356a8f9bc3b62f4080bf5275a1bbd3c7c6e
Kacper Korzeniewski <93936431+ItatchiUchicha@users.noreply.github.com>
2022-03-12 01:27:46 +0100

    Add files via upload

---
+++ Zadania domowe lab2.py
@@ -0,0 +1,44 @@
+#Zad1 Sporty
+# lista = ['siatkówka','piłka nożna','koszykówka','hokej','kolarstwo']
+# lista.reverse()
+# print(lista)
+# lista.append('hokej')
+# lista.append('koszykówka')
+# print(lista)
+#Zad2 Słownik
+# slownik = {1:'dr',2:'mgr',3:'nr'}
+# print(slownik)
+# slownik[3] = 'numer'
+# print(slownik[3])
+#Zad3 Drugi slownik
+#slownik = {1: 'doom eternal',2: 'counter strike', 3: 'the witcher 3'}
+#print(slownik.values())
+#Zad4 Skrypt
+#napis = input()
+# print(napis.count('a'))
+#Zad5 Drugi skrypt
+# a = input("podaj 1 liczbe: ")
+# b = input("podaj 2 liczbe: ")
+# c = input("podaj 3 liczbe: ")
+# a = int(a)
+# b = int(b)
+# c = int(c)
+#print(a ** b + c)
+#Zad6 Trzeci skrypt
+# a = input("podaj 1 liczbe: ")
+# b = input("podaj 2 liczbe: ")
+# c = input("podaj 3 liczbe: ")
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# print("Najwieksza liczba to: ",max(a,b,c))
+#Zad7 Czwarty skrypt
+# import math
+# lista = [1,2,3,4.5,5.2,7.3]
+# for x in lista:
+#     print(x ** 2)
+#Zad9 Piąty skrypt
+# import math
+# a =  input("Poadj liczbe: ")
+# a = int(a)
+# print(math.sqrt(a))
